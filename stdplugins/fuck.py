4c589746b184c6851d84080f26ab9f3c1a550409
lordvader98 <53733253+lordvader98@users.noreply.github.com>
2019-09-05 12:57:34 +0530

    Update fuck.py

--- stdplugins/fuck.py
+++ stdplugins/fuck.py
@@ -2,7 +2,8 @@
 Available Commands
 .fak
 .sux
-.kess"""
+.kess
+.muth"""
 
 from telethon import events
 
@@ -122,3 +123,53 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 4])
+            
+            
+           
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.2
+
+    animation_ttl = range(0, 101)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "muth":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "✊️       ",
+
+            "  ✊️",
+
+            "    ✊️",
+
+            "       ✊️",
+
+            "    ✊️",
+
+            "  ✊️",
+
+            "✊️       ",
+
+            "✊️💦",
+
+            "✊️💦💦",
+
+            "✊️💦💦💦"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+await event.edit(animation_chars[i % 10])
